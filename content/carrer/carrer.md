098ea21c431ee6b021397c3c711f686f569473e1
EnzoVezzaro <enzo-vezzaro@live.it>
2017-10-05 17:17:27 +0200

    fixed various bugs

---
+++ content/carrer/carrer.md
@@ -0,0 +1,13 @@
+{
+    "title": "Carrer",
+    "description": "Positions Available",
+    "tags": [ "carrer" ],
+    "date": "2017-07-10",
+    "categories": [
+        "Carrer"
+    ],
+    "slug": "carrer",
+    "image": ""
+}
+
+No position currently available
--- static/js/custom.js
+++ static/js/custom.js
@@ -3,8 +3,44 @@
 
 //var parallax = new Parallax(scene);
 
-$(document).ready( function() {
+console.log("Here");
 
+$(document).ready(function(){
+
+  $('.slick--carousel').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        arrows: false
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
 
 
 });
